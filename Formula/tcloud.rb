2b7e986e5aef6e551d2a8b99df41e8ffc7e69e8b
Thomas Kooi <thomas@thalassa.cloud>
2025-03-27 23:01:07 +0100

    update tcloud version
    Signed-off-by: Thomas Kooi <thomas@thalassa.cloud>

--- Formula/tcloud.rb
+++ Formula/tcloud.rb
@@ -9,7 +9,7 @@ class Tcloud < Formula
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/thalassa-cloud/cli/releases/download/v0.0.1/tcloud_0.0.1_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "94d79459a644dfff626366567cd2688ec25f5245b592fedff519ca623d7ec3e9"
+        sha256 "2fad1ace757192970739306ab84ebe94fa1972036d339f441502edf26c0aba9e"
   
         def install
           bin.install "tcloud"
@@ -20,7 +20,7 @@ class Tcloud < Formula
       end
       if Hardware::CPU.intel?
         url "https://github.com/thalassa-cloud/cli/releases/download/v0.0.1/tcloud_0.0.1_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "bc9dcad6e86a84d97398aacae1388e1d8aaf369324de46a1f27b4ab6f6d5e31e"
+        sha256 "c1d4fe5ae38de679cc2818d69b6703acb6640f9fa38429246fd147b460786801"
   
         def install
           bin.install "tcloud"
@@ -34,7 +34,7 @@ class Tcloud < Formula
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/thalassa-cloud/cli/releases/download/v0.0.1/tcloud_0.0.1_linux_arm64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "7ac1fb7734b37ec548190bf82b19ca29a0511a6c536b4bf75698b978beb306bb"
+        sha256 "12a96a2d5a029ed138d4192247c184e1414728cb82a669cc08ecb7365b7614a6"
   
         def install
             bin.install "tcloud"
@@ -45,7 +45,7 @@ class Tcloud < Formula
       end
       if Hardware::CPU.intel?
         url "https://github.com/thalassa-cloud/cli/releases/download/v0.0.1/tcloud_0.0.1_linux_amd64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "0cb3c817452e8b564fc3366a42a6213e09efb63ff8759de10172defcffe76ff6"
+        sha256 "de84b719a119fe5c0f34fab3be4b86fe5c7baecfa376bad136ef34b7bdbd6d63"
   
         def install
           bin.install "tcloud"
